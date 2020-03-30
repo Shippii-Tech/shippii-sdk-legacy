7e8ea4c5aa1edd34bebe1cf972281033208e4d04
Petyo Tsonev <p.tsonev@shippii.com>
2020-03-30 16:30:59 +0300

    Added the shopify helper

---
+++ src/Support/Shopify.php
@@ -0,0 +1,52 @@
+<?php
+namespace Shippii\Support;
+
+use Shippii\Shippii;
+
+/**
+ * Class Shopify
+ * @package Shippii\Support
+ */
+class Shopify
+{
+    /**
+     * @var Shippii
+     */
+    private $shippii;
+
+    /**
+     * Shopify constructor.
+     * @param  Shippii  $shippii
+     */
+    public function __construct(Shippii $shippii)
+    {
+        $this->shippii = $shippii;
+    }
+
+    /**
+     * @param  int  $shopifyOrderId
+     * @return array
+     * @throws \Shippii\Exceptions\Auth\ShippiiAuthenticationException
+     * @throws \Shippii\Exceptions\Auth\ShippiiAuthorizationException
+     * @throws \Shippii\Exceptions\ShippiiEndpointNotFoundException
+     * @throws \Shippii\Exceptions\ShippiiServerErrorException
+     * @throws \Shippii\Exceptions\ShippiiValidationException
+     * @deprecated
+     */
+    public function notifyShopifyForFailedImport(int $shopifyOrderId): array
+    {
+        $requestData = new TightencoCollection();
+        $requestData->put('json', [
+            'shopify_order_id' => $shopifyOrderId
+        ]);
+
+        $response = $this->shippii->connector->request(
+            'post',
+            'temporary/handle-magento-shopify-failed',
+            'v2',
+            $requestData
+        );
+
+        return $response->toArray();
+    }
+}
